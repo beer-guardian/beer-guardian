ee2cd7068037de9dbb1bf59b498f1afaf8bc0ecf
Daniel Dyla <dyladan@gmail.com>
2017-09-30 13:15:41 -0400

    Move index to templates

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, "views"));
 
 // All files in public are served directly
-app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use('/', express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => res.render("index"));
 
 // JSON api in the api folder
 app.use("/api/v1", require("./api"));
