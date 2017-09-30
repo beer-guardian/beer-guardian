563d90d478df94213f0d1a52385c61e95aa48146
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2017-09-30 14:24:26 -0400

    tilt js

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,7 +2,19 @@
   
     
     $(function() {
-      console.log(true)
+
+
+      //init tilt js cards with options
+      const tilt = $('.js-tilt').tilt({
+        glare: true,
+        maxGlare: 0.6,
+        speed: 400,
+        scale: 1.1,
+        maxTilt: 10,
+        perspective: 500
+      });
+
+      
     });
     
     
