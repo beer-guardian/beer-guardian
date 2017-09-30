277b70bfbf67534aee1675ccfc38b576813200ca
Daniel Dyla <dyladan@gmail.com>
2017-09-30 13:07:11 -0400

    Basic express setup

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = require("express").Router();
+
+module.exports.get("/", (req, res) => {
+  res.json({ ok: "you found me" });
+});
+
---
+++ src/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const path = require("path");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const hbs = require("express-hbs");
+app.engine('hbs', hbs.express4({ defaultLayout: path.join(__dirname, "views/main"), extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, "views"));
+
+// All files in public are served directly
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// JSON api in the api folder
+app.use("/api/v1", require("./api"));
+
+// Render views directly
+app.use("/template", (req, res) => {
+  res.render("template");
+});
+
+console.log(`server is listening on http://localhost:${port}`)
+app.listen(port);
+
