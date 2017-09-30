15f981df2a94a9271dd6e512936b54025ce0591e
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2017-09-30 17:07:42 -0400

    beer cards and modal start

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,28 +1,47 @@
 (function() {
   
+
     
     $(function() {
 
+      //color the negative scores in red
+      scoreColor()
 
       //init tilt js cards with options
       const tilt = $('.js-tilt').tilt({
         glare: true,
-        maxGlare: 0.6,
+        maxGlare: 0.3,
         speed: 400,
-        scale: 1.1,
+        scale: 1,
         maxTilt: 10,
         perspective: 500
       });
 
-      
+
+      $('.beer-cards .card').click(function() {
+        openModal()
+      })
+
+
     });
     
     
     
     
+    function openModal() {
+
+    }
     
-    
-    
+
+    function scoreColor() {
+      $('.card .score span').each(function() {
+        var parseScore = parseFloat($(this).text());
+        if (parseScore < 0) {
+          console.log(true)
+          $(this).addClass('negative')
+        }
+      });
+    }
     
     
     
