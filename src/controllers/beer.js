71e53949353bb0d12f01629564ee056496d05893
Daniel Dyla <dyladan@gmail.com>
2017-09-30 22:58:42 -0400

    Fix score output

--- src/controllers/beer.js
+++ src/controllers/beer.js
@@ -10,17 +10,18 @@ class BeerController {
         Promise.all(beers.map((beer) =>
           Votes.getVotesByBeer(beer._id)
             .then((votes) => {
+              const out = beer.toJSON();
               const up = votes.filter(v => v.up).length;
-              beer.up = up;
-              beer.down = votes.length - up;
-              beer.score = beer.up - beer.down;
+              out.up = up;
+              out.down = votes.length - up;
+              out.score = out.up - out.down;
               if (user) {
                 const myVote = votes.filter(v => v.user.equals(user._id))[0];
                 if (myVote) {
-                  beer.userVote = myVote.up ? "UP" : "DOWN";
+                  out.userVote = myVote.up ? "UP" : "DOWN";
                 }
               }
-              return beer;
+              return out;
             }))));
   }
 }
